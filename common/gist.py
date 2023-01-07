aa8c945922319bb130123618f4d513b258392710
Lukas Kotlik <kotliluk@fel.cvut.cz>
2023-01-07 12:48:15 +0100

    Make git commands silent.

--- common/gist.py
+++ common/gist.py
@@ -5,25 +5,16 @@ GIST_URL = 'https://gist.github.com/ff1d1749b91389cc1f0fdb5c6d7ec580.git'
 GIST_DIR_NAME = 'ff1d1749b91389cc1f0fdb5c6d7ec580'
 
 
-def clone_gist():
-    print('Cloning Gist from Git...', flush=True)
-    call(f'git submodule add --force {GIST_URL}')
-
-
 def pull_gist():
-    print('Pulling Gist from Git...', flush=True)
-    call(f'cd {GIST_DIR_NAME} && git fetch')
-    call(f'cd {GIST_DIR_NAME} && git pull')
+    call(f'cd {GIST_DIR_NAME} && git fetch -q')
+    call(f'cd {GIST_DIR_NAME} && git pull -q')
 
 
 def push_gist():
-    print('Pushing Gist to Git...', flush=True)
-    call(f'cd {GIST_DIR_NAME} && git add . && git commit -m "msg" && git push')
+    call(f'cd {GIST_DIR_NAME} && git add . && git commit -m "msg" -q && git push -q')
 
 
 def init_gist():
     print('Cloning Gist from Git...', flush=True)
     call(f'git submodule add --force {GIST_URL}')
-    print('Updating Gist from Git...', flush=True)
-    call(f'cd {GIST_DIR_NAME} && git fetch')
-    call(f'cd {GIST_DIR_NAME} && git pull')
+    pull_gist()
