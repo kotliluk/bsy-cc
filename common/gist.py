0d7f91bc5a4e31c69c4a437dddd639a96d64fb16
Lukas Kotlik <kotliluk@fel.cvut.cz>
2023-01-07 13:22:01 +0100

    Add git push retrying.

--- common/gist.py
+++ common/gist.py
@@ -1,4 +1,5 @@
 from os import system as call
+from time import sleep
 
 
 GIST_URL = 'https://gist.github.com/ff1d1749b91389cc1f0fdb5c6d7ec580.git'
@@ -8,10 +9,18 @@ GIST_DIR_NAME = 'ff1d1749b91389cc1f0fdb5c6d7ec580'
 def pull_gist():
     call(f'cd {GIST_DIR_NAME} && git fetch -q')
     call(f'cd {GIST_DIR_NAME} && git pull -q')
+    print(flush=True)
 
 
 def push_gist():
-    call(f'cd {GIST_DIR_NAME} && git add . && git commit -m "msg" -q && git push -q')
+    r = call(f'cd {GIST_DIR_NAME} && git add . && git commit -m "msg" -q && git push -q')
+
+    while r != 0:
+        # retry in case of parallel git process
+        sleep(2)
+        r = call(f'cd {GIST_DIR_NAME} && git push -q')
+
+    print(flush=True)
 
 
 def init_gist():
