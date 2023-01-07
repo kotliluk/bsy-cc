bb37c31e8c6cc412414cd3f708dae8ba836734bf
Lukas Kotlik <kotliluk@fel.cvut.cz>
2023-01-07 17:22:47 +0100

    Git pull before first push try.

--- common/gist.py
+++ common/gist.py
@@ -10,12 +10,17 @@ def pull_gist():
 
 
 def push_gist():
-    r = call(f'cd {GIST_DIR_NAME} && git add . && git commit -m "msg" -q && git push -q')
+    call(f'cd {GIST_DIR_NAME} && git add . && git commit -m "msg" -q')
+    pull_gist()
+    r = call(f'cd {GIST_DIR_NAME} && git push -q')
 
     while r != 0:
-        print('Please wait, retrying to push to git...')
+        print('Please wait, retrying to push to git...', flush=True)
         pull_gist()
         r = call(f'cd {GIST_DIR_NAME} && git push -q')
+
+        if r == 0:
+            print('Successfully pushed to git', flush=True)
 
 
 def init_gist():
